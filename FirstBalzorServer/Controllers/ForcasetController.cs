e426f842f717b28db4c8c9cb9cbc0d0e45143c81
ParkerS00 <parkerswenson00@gmail.com>
2023-10-12 09:46:22 -0600

    Class Prep 10/12/2023

---
+++ FirstBalzorServer/Controllers/ForcasetController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using FirstBalzorServer.Data;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstBalzorServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ForcasetController : ControllerBase
+    {
+        public async Task<ActionResult<IEnumerable<WeatherForecast>>> GetAll()
+        {
+            var svc = new WeatherForecastService();
+
+            return new OkObjectResult(await svc.GetForecastAsync(DateTime.Now)); 
+        }
+    }
+}
